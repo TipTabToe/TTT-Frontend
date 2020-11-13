7b7b9ac7bc7385ecf35011d7230717073b02f10e
kanerla <kanervalaurajohanna@gmail.com>
2020-11-13 17:19:27 +0200

    Add GlobalClass, show answer options in buttons

--- Assets/Scenes/Game/GameScript.cs
+++ Assets/Scenes/Game/GameScript.cs
@@ -1,21 +1,35 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
-public class GameScript : MonoBehaviour
-{
+public class GameScript : MonoBehaviour {
+    public TMP_Text questionText;
+    private QuestionSet myObject;
+    public TMP_Text btnText1;
+    public TMP_Text btnText2;
+    public TMP_Text btnText3;
+    public TMP_Text btnText4;
+    public Button btn1;
+    public Button btn2;
+    public Button btn3;
+    public Button btn4;
+
+
     // Start is called before the first frame update
-    void Start()
-    {
-        StartCoroutine(RequestRoutine("localhost:8080/api/questions", ResponseCallback));
+    void Start() {
+        StartCoroutine(RequestRoutine(GlobalClass.API_URL + "/questions", ResponseCallback));
+        btn1.onClick.AddListener(delegate { Clicked(0); });
+        btn2.onClick.AddListener(delegate { Clicked(1); });
+        btn3.onClick.AddListener(delegate { Clicked(2); });
+        btn4.onClick.AddListener(delegate { Clicked(3); });
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
     }
 
     // Keep track of what we got back
@@ -24,31 +38,40 @@ public class GameScript : MonoBehaviour
     // Web requests are typially done asynchronously, so Unity's web request system
     // returns a yield instruction while it waits for the response.
     //
-    private IEnumerator RequestRoutine(string url, Action<string> callback = null)
-    {
+    private IEnumerator RequestRoutine(string url, Action<string> callback = null) {
         // Using the static constructor
         var request = UnityWebRequest.Get(url);
- 
+
         // Wait for the response and then get our data
         yield return request.SendWebRequest();
         var data = request.downloadHandler.text;
- 
+
         // This isn't required, but I prefer to pass in a callback so that I can
         // act on the response data outside of this function
         if (callback != null)
             callback(data);
     }
- 
+
     // Callback to act on our response data
-    private void ResponseCallback(string data)
-    {
+    private void ResponseCallback(string data) {
         Debug.Log(data);
         recentData = data;
-        
-        var myObject = JsonUtility.FromJson<QuestionSet>("{\"questions\":" + data + "}");
+
+        myObject = JsonUtility.FromJson<QuestionSet>("{\"questions\":" + data + "}");
         Debug.Log(myObject.questions);
         foreach (var q in myObject.questions) {
             Debug.Log(q.question);
         }
+
+        Question question = myObject.questions[1];
+        questionText.text = question.question;
+        btnText1.text = question.answers[0];
+        btnText2.text = question.answers[1];
+        btnText3.text = question.answers[2];
+        btnText4.text = question.answers[3];
+    }
+
+    private void Clicked(int num) {
+        Debug.Log(num);
     }
 }
---
+++ Assets/Scenes/GlobalClass.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GlobalClass {
+    public const string API_URL = "localhost:8080/api";
+}

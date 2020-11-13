353d453c1a4566542ea0924963f2a9de6de8756a
Sannisuvi <sanni.kytola@hotmail.com>
2020-11-13 15:38:49 +0200

    Add Score scene

---
+++ Assets/Scenes/Score/ScoreSceneScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScoreSceneScript : MonoBehaviour {
+
+    public TMP_Text heading;
+    public TMP_Text points;
+    public TMP_Text opponentPoints;
+    
+    // Start is called before the first frame update
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
+    }
+}

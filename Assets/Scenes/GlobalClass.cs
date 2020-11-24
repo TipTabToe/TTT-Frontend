c72be5df8ee6233fb8900e57f2da3de8d49f596e
Heikki Kangas <heikki.kangas@tuni.fi>
2020-11-24 03:43:36 +0200

    Update API url

--- Assets/Scenes/GlobalClass.cs
+++ Assets/Scenes/GlobalClass.cs
@@ -3,6 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public static class GlobalClass {
-    public const string API_URL = "localhost:8080/api";
+    public const string API_URL = "dradge.xyz:3500/api";
     public static User player;
 }

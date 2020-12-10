8f68ed637c5e4384a132d4f9e623fc31ea9b9f9f
kanerla <kanervalaurajohanna@gmail.com>
2020-12-10 15:42:29 +0200

    Change API_URL to AWS

--- Assets/Scenes/GlobalClass.cs
+++ Assets/Scenes/GlobalClass.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 // This is used to save all the important information
 public static class GlobalClass {
-    public const string API_URL = "dradge.xyz:3500/api";
+    public const string API_URL = "http://ec2-18-192-24-188.eu-central-1.compute.amazonaws.com/api";
     public static User player;
     public static int lastRoundPoints = 0;
     public static int category = 1;
